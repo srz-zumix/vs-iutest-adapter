064d8cde67c4c1eb893bb0c96df30c4b0727bfa8
srz_zumix <zumix.cpp@gmail.com>
2015-09-06 22:51:23 +0900

    ひな形作成

---
+++ iutest_adapter/iutestTestDiscoverer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+
+namespace iutest_adapter
+{
+    [FileExtension(".dll")]
+    [FileExtension(".exe")]
+    [DefaultExecutorUri(iutestTestExecutor.ExecutorUriString)]
+    class iutestTestDiscoverer : ITestDiscoverer
+    {
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext
+            , IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        {
+            foreach( string s in sources )
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+    }
+}
---
+++ iutest_adapter/iutestTestExecutor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace iutest_adapter
+{
+    public class iutestTestExecutor : ITestExecutor
+    {
+        public void Cancel()
+        {
+
+        }
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+
+        }
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+
+        }
+
+        public const string ExecutorUriString = "executor://iutestTestExecutor/v1";
+        public static readonly Uri ExecutorUri = new Uri(iutestTestExecutor.ExecutorUriString);
+    }
+}
