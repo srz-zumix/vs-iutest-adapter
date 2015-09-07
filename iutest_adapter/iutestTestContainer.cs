c75d554a1c7ccd544f470b2f83b8495e4bf9226f
srz_zumix <zumix.cpp@gmail.com>
2015-09-08 00:19:36 +0900

    ひな形

---
+++ iutest_adapter/iutestTestContainer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestWindow.Extensibility;
+using Microsoft.VisualStudio.TestWindow.Extensibility.Model;
+
+namespace iutest_adapter
+{
+    public class iutestTestContainer : ITestContainer
+    {
+        private ITestContainerDiscoverer discoverer;
+        public iutestTestContainer(ITestContainerDiscoverer discoverer, string source, Uri executorUri)
+            :this(discoverer, source, executorUri, Enumerable.Empty<Guid>())
+        { }
+        public iutestTestContainer(ITestContainerDiscoverer discoverer, string source, Uri executorUri, IEnumerable<Guid> debugEngines)
+        {
+            this.Source = source;
+            this.ExecutorUri = executorUri;
+            this.DebugEngines = debugEngines;
+            this.discoverer = discoverer;
+            this.TargetFramework = FrameworkVersion.None;
+            this.TargetPlatform = Architecture.AnyCPU;
+            this.timeStamp = GetTimeStamp();
+        }
+
+        private iutestTestContainer(iutestTestContainer copy)
+            : this(copy.discoverer, copy.Source, copy.ExecutorUri)
+        {
+            this.timeStamp = copy.timeStamp;
+        }
+
+        private DateTime GetTimeStamp()
+        {
+            if (!String.IsNullOrEmpty(this.Source) && File.Exists(this.Source))
+            {
+                return File.GetLastWriteTime(this.Source);
+            }
+            else
+            {
+                return DateTime.MinValue;
+            }
+
+        }
+
+        private readonly DateTime timeStamp;
+
+        public string Source { get; set; }
+        public Uri ExecutorUri { get; set; }
+        public IEnumerable<Guid> DebugEngines { get; set; }
+        public FrameworkVersion TargetFramework { get; set; }
+        public Architecture TargetPlatform { get; set; }
+        public override string ToString()
+        {
+            return this.ExecutorUri.ToString() + "/" + this.Source;
+        }
+        public IDeploymentData DeployAppContainer() { return null; }
+        public bool IsAppContainerTestContainer { get { return false; } }
+        public ITestContainerDiscoverer Discoverer { get { return discoverer; } }
+
+        public int CompareTo(ITestContainer other)
+        {
+            var testContainer = other as iutestTestContainer;
+            if (testContainer == null)
+            {
+                return -1;
+            }
+
+            var result = String.Compare(this.Source, testContainer.Source, StringComparison.OrdinalIgnoreCase);
+            if (result != 0)
+            {
+                return result;
+            }
+
+            return this.timeStamp.CompareTo(testContainer.timeStamp);
+        }
+
+        public ITestContainer Snapshot()
+        {
+            return new iutestTestContainer(this);
+        }
+    }
+}
---
+++ iutest_adapter/iutestTestContainerDiscoverer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestWindow.Extensibility;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+
+namespace iutest_adapter
+{
+    [Export (typeof(ITestContainerDiscoverer))]
+    class iutestTestContainerDiscoverer : ITestContainerDiscoverer
+    {
+        public const string ExecutorUriString = "executor:/iutestTestExecutor/v1";
+
+        public event EventHandler TestContainersUpdated;
+
+        private bool initialContainerSearch;
+        private readonly List<ITestContainer> cachedContainers;
+
+        public Uri ExecutorUri { get { return new System.Uri(ExecutorUriString); } }
+        public IEnumerable<ITestContainer> TestContainers { get { return GetTestContainers(); } }
+
+        private IEnumerable<ITestContainer> GetTestContainers()
+        {
+            if (initialContainerSearch)
+            {
+                cachedContainers.Clear();
+                initialContainerSearch = false;
+            }
+
+            return cachedContainers;
+        }
+    }
+}
